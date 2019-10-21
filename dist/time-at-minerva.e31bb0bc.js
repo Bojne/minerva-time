b32b06b3b280f29658e66ca81fd39e439736f687
岳涵黃 <yuehanhuang@yuehanhuangde-MacBook-Pro.local>
2019-10-21 14:19:54 +0900

    Change the responsiveness of font size

--- dist/time-at-minerva.e31bb0bc.js
+++ dist/time-at-minerva.e31bb0bc.js
@@ -37989,7 +37989,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59234" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51652" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
